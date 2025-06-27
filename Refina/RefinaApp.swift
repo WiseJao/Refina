b54191d687f035550c7b440edf60eac49d38f91d
WiseGhostan <wiseghostan@protonmail.com>
2025-06-27 16:52:56 -0300

    Initial Commit

---
+++ Refina/RefinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefinaApp.swift
+//  Refina
+//
+//  Created by Wise on 27/06/25.
+//
+
+import SwiftUI
+
+@main
+struct RefinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
